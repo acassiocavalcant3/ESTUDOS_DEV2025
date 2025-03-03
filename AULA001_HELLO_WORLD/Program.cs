a5f9348b1de19e9590326efd7d28e0c753be8474
Acassio Cavalcante <81931338+acassiocavalcant3@users.noreply.github.com>
2025-03-03 19:02:58 -0400

    criei o repositorio e adicionei helloworld

---
+++ AULA001_HELLO_WORLD/Program.cs
@@ -0,0 +1,17 @@
+﻿back1:
+//DESCRICAO: MEU PRIMEIRO ALGORITMO
+Console.WriteLine("\n\t***HELLO WORLD***\n"); //ESCREVE NA TELA HELLO WORD, COM NOVA LINHA E TAB
+
+Console.WriteLine("\nDeseja executar novamente? [s/n]");
+char escolha = char.Parse(Console.ReadLine());
+if (escolha == 'S' || escolha == 's')
+{
+    Console.Clear();
+    goto back1;
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("\nFim da execução. Tecle enter para sair... \n");
+    Console.ReadLine();
+}
